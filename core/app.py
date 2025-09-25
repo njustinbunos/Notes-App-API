2f970f17ca3059be757cc5e4247bd2fa83c58bc8
njustinbunos <justinjairebanzuela.bunos25@bicol-u.edu.ph>
2025-09-25 15:40:38 +0800

    add: some routes to be implemented and CORS middleware code.

--- core/app.py
+++ core/app.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
-from fastapi.middleware import cors
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    #allow_origins=["http://localhost:3000"],  # Or whathever the host of my React frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def read_root():
     return {"Message":"Hello World!"}
+
+@app.get("/notes/")
+async def get_notes():
+    return {"Notes":"a list of notes or smth"}
+
+@app.post("/notes/")
+async def create_notes():
+    #create notes
+    pass

d7105877b45566cd279964e2001ecc6baeb8e7e1
njustinbunos <justinjairebanzuela.bunos25@bicol-u.edu.ph>
2025-09-25 20:26:44 +0800

    add: func to initialize db upon startup

--- core/app.py
+++ core/app.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Path, Query, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from database.connection import initialize_db, get_session
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    initialize_db()
+    yield   
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
@@ -10,7 +19,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 @app.get("/")
 async def read_root():

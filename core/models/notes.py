f2312b36087058ef02b61f314a610c5d6571ff1d
njustinbunos <justinjairebanzuela.bunos25@bicol-u.edu.ph>
2025-09-26 19:49:28 +0800

    refactor: rewrite Note into base + schemas for more practical use.

--- core/models/notes.py
+++ core/models/notes.py
@@ -2,12 +2,25 @@ from sqlmodel import SQLModel, Field, Relationship
 from users import User
 from typing import Optional
 
-class Note(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+class NoteBase(SQLModel):
     body: str
-    colors: str
+    color: str  
     position: str
 
-    # Foreign key
-    owner_id: int = Field(foreign_key="user.id")
-    owner: "User" = Relationship(back_populates="notes")
+class Note(NoteBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    owner_id: Optional[int] = Field(default=None, foreign_key="user.id")
+    owner: Optional["User"] = Relationship(back_populates="notes")
+
+class NoteCreate(NoteBase):
+    pass
+
+class NoteRead(NoteBase):
+    id: int
+    owner_id: Optional[int]
+
+# Input: when updating
+class NoteUpdate(SQLModel):
+    body: Optional[str] = None
+    color: Optional[str] = None  
+    position: Optional[str] = None

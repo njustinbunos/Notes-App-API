35acc7d8ee2f02ca59dcb5c1fdae779fa691461a
njustinbunos <justinjairebanzuela.bunos25@bicol-u.edu.ph>
2025-09-26 21:50:52 +0800

    feat: implemented POST `/notes/` endpoint

--- core/app.py
+++ core/app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Depends, Path, Query, HTTPException
+from sqlmodel import Session, select
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
+from models import *
 
 from database.connection import initialize_db, get_session
 
@@ -24,11 +26,15 @@ app.add_middleware(
 async def read_root():
     return {"Message":"Hello World!"}
 
+@app.post("/notes/", response_model=Note)
+async def create_notes(note: NoteCreate, session: Session = Depends(get_session)):
+    db_note = Note.model_validate(note)
+    session.add(db_note)
+    session.commit()
+    session.refresh(db_note)
+    return db_note
+
 @app.get("/notes/")
 async def get_notes():
     return {"Notes":"a list of notes or smth"}
 
-@app.post("/notes/")
-async def create_notes():
-    #create notes
-    pass

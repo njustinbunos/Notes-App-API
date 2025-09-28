1037e267f9d57a6f087029588fd26aa7c36b9ba6
njustinbunos <justinjairebanzuela.bunos25@bicol-u.edu.ph>
2025-09-28 14:52:25 +0800

    feat: added stricter validation rules

--- core/models.py
+++ core/models.py
@@ -1,24 +1,25 @@
 from sqlmodel import SQLModel, Field, Relationship
-from typing import Optional, Dict, List
+from typing import Optional, List, Dict
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    username: str = Field(index=True, unique=True, nullable=False)
-    email: str = Field(index=True, unique=True, nullable=False)
+    username: str = Field(index=True, unique=True, nullable=False, min_length=3, max_length=30)
+    email: str = Field(index=True, unique=True, nullable=False) 
     password_hash: str = Field(nullable=False)
 
-    # db relationship, one user can have many notes
     notes: List["Note"] = Relationship(back_populates="owner")
 
 
 class NoteBase(SQLModel):
-    body: str
-    color_id: str
-    color_header: str
-    color_body: str
-    color_text: str
-    pos_x: int
-    pos_y: int
+    body: str = Field(min_length=1, max_length=500)
+    color_id: str = Field(max_length=20)
+
+    color_header: str = Field(max_length=7, regex="^#(?:[0-9a-fA-F]{3}){1,2}$")  # Hex color code
+    color_body: str = Field(max_length=7, regex="^#(?:[0-9a-fA-F]{3}){1,2}$")  # Hex color code
+    color_text: str = Field(max_length=7, regex="^#(?:[0-9a-fA-F]{3}){1,2}$")  # Hex color code
+    
+    pos_x: int = Field(ge=0, le=5000)
+    pos_y: int = Field(ge=0, le=5000)
 
 
 class Note(NoteBase, table=True):
@@ -26,15 +27,21 @@ class Note(NoteBase, table=True):
     owner_id: Optional[int] = Field(default=None, foreign_key="user.id")
     owner: Optional["User"] = Relationship(back_populates="notes")
 
+
 class NoteCreate(NoteBase):
     pass
+
 
 class NoteRead(NoteBase):
     id: int
     owner_id: Optional[int]
 
-# Input: when updating
+
 class NoteUpdate(SQLModel):
-    body: Optional[str] = None
-    color: Optional[str] = None  
-    position: Optional[str] = None
+    body: Optional[str] = Field(default=None, min_length=1, max_length=500)
+    color_id: Optional[str] = None
+    color_header: Optional[str] = None
+    color_body: Optional[str] = None
+    color_text: Optional[str] = None
+    pos_x: Optional[int] = Field(default=None, ge=0, le=5000)
+    pos_y: Optional[int] = Field(default=None, ge=0, le=5000)
